7c007407c1da38502010dd5f5e991e2e640432f3
Simone Drogo <s.drogo@campus.unimib.it>
2025-06-11 18:02:02 +0200

    Aggiunta HashMap per il track delle variabili

---
+++ client/src/main/java/org/evosuite/testcase/VariabiliTracker.java
@@ -0,0 +1,29 @@
+package org.evosuite.testcase;
+
+import java.util.HashMap;
+
+public class VariabiliTracker {
+    private HashMap<String, String> variabili;
+
+    public VariabiliTracker() {
+        variabili = new HashMap<>();
+    }
+
+    // Aggiunge una variabile, ma segnala errore se il nome esiste già
+    public void aggiungiVariabile(String nome, String tipo) {
+        if (variabili.containsKey(nome)) {
+            throw new IllegalArgumentException("La variabile '" + nome + "' è già stata dichiarata.");
+        }
+        variabili.put(nome, tipo);
+    }
+
+    public String getTipo(String nome) {
+        return variabili.get(nome);
+    }
+
+    public void stampaVariabili() {
+        for (String nome : variabili.keySet()) {
+            System.out.println(nome + " -> " + variabili.get(nome));
+        }
+    }
+}
